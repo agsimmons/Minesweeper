44bdd05feb68e92e723ecba4d71743d932f9e0a1
Brendan Sileo <brendansileo@gmail.com>
2018-07-02 11:06:53 -0700

    Added inputBoardWidth procedure

--- minesweeper.asm
+++ minesweeper.asm
@@ -5,13 +5,31 @@ INCLUDE irvine32.inc
 	; Width of game board
 	boardWidth dd 1 DUP(?)
 
+	;Text messages
+	widthRequest db "Choose your board width. It can be any number between 1 and 20.",0
 .code
 main proc
-
 	; Code Here
 	exit
 
 main endp
+
+;Inputs:
+;	None
+;Outputs:
+;	boardWidth: Width of the Game Board
+inputBoardWidth proc 
+	push eax
+	push edx
+	mov edx, offset widthRequest
+	call WriteString
+	call Crlf
+	call ReadInt
+	mov boardwidth, eax
+	pop edx
+	pop eax
+	ret
+inputBoardWidth endp
 
 ;Inputs:
 ;	eax: Y Coordinate
