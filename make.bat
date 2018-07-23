5fcaeeed852d85bfe352c515b6ac514f94bb6782
Ethan Smith <smithe4@wit.edu>
2018-07-23 14:03:17 -0400

    make works for everyone

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
 
 
-C:\Irvine\masm32\bin\ml.exe /nologo /c /Zi /coff %1.asm
-C:\Irvine\masm32\bin\link.exe /NOLOGO /SUBSYSTEM:CONSOLE %1.obj
+C:\masm32\bin\ml.exe /nologo /c /Zi /coff %1.asm
+C:\masm32\bin\link.exe /NOLOGO /SUBSYSTEM:CONSOLE %1.obj
